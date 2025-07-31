39a944e775d8d864fa38fe6713ff352987b8458d
GuilhermeMenez <guilhermetav@edu.unirio.br>
2025-07-30 22:32:50 -0300

    Add unit tests for `AuthorizationService` and `SecurityFilter`
    Introduced comprehensive unit tests for `AuthorizationService` covering success, email already registered, and authentication failure scenarios. Added a test case for `SecurityFilter` to validate request forwarding through the filter chain.

--- src/test/java/blog/code/codeblog/security/config/SecurityFilterTest.java
+++ src/test/java/blog/code/codeblog/security/config/SecurityFilterTest.java
@@ -1,12 +1,40 @@
 package blog.code.codeblog.security.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 
 class SecurityFilterTest {
 
+    private SecurityFilter securityFilter;
+    private FilterChain filterChain;
+
+    @BeforeEach
+    @DisplayName("Configuração inicial do SecurityFilter")
+    void setup() {
+        securityFilter = new SecurityFilter();
+        filterChain = Mockito.mock(FilterChain.class);
+    }
+
     @Test
-    void doFilterInternal() {
+    @DisplayName("Executa o filtro com uma requisição válida e deve prosseguir no fluxo")
+    void doFilterInternal_withValidRequest_shouldProceed() throws ServletException, IOException {
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpServletResponse response = new MockHttpServletResponse();
+
+        securityFilter.doFilterInternal(request, response, filterChain);
+
+        verify(filterChain, times(1)).doFilter(request, response);
     }
 }
--- src/test/java/blog/code/codeblog/service/AuthorizationServiceTest.java
+++ src/test/java/blog/code/codeblog/service/AuthorizationServiceTest.java
@@ -1,20 +1,85 @@
 package blog.code.codeblog.service;
 
+import blog.code.codeblog.dto.UserDTO;
+import blog.code.codeblog.enums.UserRoles;
+import blog.code.codeblog.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-class AuthorizationServiceTest {
+public class AuthorizationServiceTest {
 
-    @Test
-    void loadUserByUsername() {
+    @InjectMocks
+    private AuthorizationService authorizationService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private TokenService tokenService;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void register() {
+    @DisplayName("Teste de registro - sucesso no registro")
+    public void testRegisterSuccessfulRegistration() {
+        UserDTO userDTO = new UserDTO("Test User", "test@example.com", "password", UserRoles.COSTUMER);
+        when(userService.findByLogin(userDTO.email())).thenReturn(null);
+        when(passwordEncoder.encode(userDTO.password())).thenReturn("encryptedPassword");
+        when(tokenService.generateToken(any())).thenReturn("token");
+
+        User user = new User(userDTO.name(), userDTO.email(), "encryptedPassword", userDTO.role());
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(
+                new UsernamePasswordAuthenticationToken(user, null, null)
+        );
+        String token = authorizationService.register(userDTO);
+
+        assertNotNull(token);
+        assertEquals("token", token);
+        verify(userService, times(1)).saveUser(any(User.class));
     }
 
     @Test
-    void login() {
+    @DisplayName("Teste de registro - email já registrado")
+    public void testRegisterEmailAlreadyRegistered() {
+        UserDTO userDTO = new UserDTO("Test User", "test@example.com", "password", UserRoles.COSTUMER);
+        User existingUser = new User(userDTO.name(), userDTO.email(), "encryptedPassword", userDTO.role());
+        when(userService.findByLogin(userDTO.email())).thenReturn(existingUser);
+
+        assertThrows(UsernameNotFoundException.class, () -> authorizationService.register(userDTO));
+        verify(userService, never()).saveUser(any(User.class));
+    }
+
+    @Test
+    @DisplayName("Teste de registro - falha na autenticação")
+    public void testRegisterAuthenticationFails() {
+        UserDTO userDTO = new UserDTO("Test User", "test@example.com", "password", UserRoles.COSTUMER);
+        when(userService.findByLogin(userDTO.email())).thenReturn(null);
+        when(passwordEncoder.encode(userDTO.password())).thenReturn("encryptedPassword");
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(BadCredentialsException.class);
+
+        assertThrows(BadCredentialsException.class, () -> authorizationService.register(userDTO));
+        verify(userService, times(1)).saveUser(any(User.class));
     }
 }
